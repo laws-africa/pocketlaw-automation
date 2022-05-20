5f82f90d684f096fe9219fdcf5bdd31af4e1acef
Robley G <robleyadrian@gmail.com>
2022-05-20 13:08:54 +0300

    use env args instead of build args, set up aws cli in bash script instead

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,10 @@
 FROM python:3.8-slim-buster
 
-ARG AWS_KEY
-ARG AWS_SECRET
-ARG AWS_REGION
-ARG PRODUCT
-ARG PRODUCT_DOMAIN
-
-ENV PRODUCT ${PRODUCT}
-ENV PRODUCT_DOMAIN ${PRODUCT_DOMAIN}
+ENV AWS_KEY=
+ENV AWS_SECRET=
+ENV AWS_REGION=
+ENV PRODUCT=
+ENV PRODUCT_DOMAIN=
 
 # install wget
 RUN apt-get update && apt-get install -y wget tree
@@ -22,11 +19,6 @@ WORKDIR /extraction
 
 # install requirements
 RUN pip install -r requirements.txt
-
-# configure aws cli
-RUN aws configure set aws_access_key_id ${AWS_KEY}
-RUN aws configure set aws_secret_access_key ${AWS_SECRET}
-RUN aws configure set region ${AWS_REGION}
 
 RUN chmod +x runner.sh
 ENTRYPOINT [ "./runner.sh" ]
--- runner.sh
+++ runner.sh
@@ -1,17 +1,51 @@
 #!/bin/sh
+set -ex
+
+# TODO: use a function
+# check for env variables
+if [ -z "$PRODUCT" ]; then
+    echo "Error: PRODUCT not set"
+    exit 2;
+fi
+
+if [ -z "$PRODUCT_DOMAIN" ]; then
+    echo "Error: PRODUCT_DOMAIN not set"
+    exit 2;
+fi
+
+if [ -z "$AWS_KEY" ]; then
+    echo "Error: AWS_KEY not set"
+    exit 2;
+fi
+
+if [ -z "$AWS_SECRET" ]; then
+    echo "Error: AWS_SECRET not set"
+    exit 2;
+fi
+
+if [ -z "$AWS_REGION" ]; then
+    echo "Error: AWS_REGION not set"
+    exit 2;
+fi
 
 ARCHIVE_FILE="./${PRODUCT}.warc.gz"
 PRODUCT_URL="https://${PRODUCT_DOMAIN}"
 MEDIA_BASE_URL="media.${PRODUCT_DOMAIN}"
 
+# configure aws cli
+aws configure set aws_access_key_id ${AWS_KEY}
+aws configure set aws_secret_access_key ${AWS_SECRET}
+aws configure set region ${AWS_REGION}
+
 # Run wget
-wget --page-requisites \
+wget --recursive --page-requisites \
     --domains ${PRODUCT_DOMAIN},${MEDIA_BASE_URL},use.fontawesome.com,fonts.googleapis.com,code.highcharts.com,cdn.jsdelivr.net \
     --span-hosts \
     --https-only --reject pdf,rtf,doc,docx,DOC \
     --delete-after \
     --warc-file=$PRODUCT \
-    --no-directories ${PRODUCT_URL}
+    --no-directories \
+    ${PRODUCT_URL}
 
 # Run warc_processor.py with location to warc file
 python /extraction/warc_processor.py --product $PRODUCT --archive ${ARCHIVE_FILE}
